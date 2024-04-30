0b91544e18209421ae3c28373b28f3ba61d46951
VishGit1234 <47926842+VishGit1234@users.noreply.github.com>
2024-04-30 14:42:35 -0400

    Change render mode

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import core
 import sac_core
 
 if __name__ == '__main__':
-    nac(lambda : gym.make("MountainCarContinuous-v0"), actor_critic=core.MLPActorCritic)
+    nac(lambda : gym.make("MountainCarContinuous-v0", render_mode='human'), actor_critic=core.MLPActorCritic)
         # start_steps=200, replay_size=100, update_every=10, update_after=200, batch_size=50, steps_per_epoch=1000)
 
     # sac(lambda : gym.make("MountainCarContinuous-v0", render_mode='human'), actor_critic=sac_core.MLPActorCritic,
